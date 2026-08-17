89085fce15bd746b0ce2a9563ea1c7747f8fd4d4
Ivano Fiorito <ivano.fiorito@gmail.com>
2026-08-17 18:03:10 +0200

    test vercel postgres big string

--- api/test_pool.ts
+++ api/test_pool.ts
@@ -1,14 +1,17 @@
-import { neon } from '@neondatabase/serverless';
+import { createPool } from '@vercel/postgres';
 
 export default async function handler(req, res) {
   try {
-    const sql = neon(process.env.DATABASE_URL);
+    const dbPool = createPool({ connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL });
+    const client = await dbPool.connect();
     
-    // Create a 50KB string
-    const bigStr = "A".repeat(50000);
-    const result = await sqlSELECT  as success;
-    res.json({ result: "success", length: result[0].success.length });
+    // Create a 150KB string
+    const bigStr = "A".repeat(150000);
+    const result = await client.query("SELECT \::text as success", [bigStr]);
+    client.release();
+    
+    res.json({ result: "success", length: result.rows[0].success.length });
   } catch(e) {
-    res.json({ error: e.message });
+    res.json({ error: e.message, stack: e.stack });
   }
 }
