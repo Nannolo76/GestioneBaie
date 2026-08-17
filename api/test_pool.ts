ee8ee638d14bb751fe55dd51c4cd258bc8bc389c
Ivano Fiorito <ivano.fiorito@gmail.com>
2026-08-17 18:05:28 +0200

    test native pg

--- api/test_pool.ts
+++ api/test_pool.ts
@@ -1,8 +1,8 @@
-import { createPool } from '@vercel/postgres';
+import { Pool } from 'pg';
 
 export default async function handler(req, res) {
   try {
-    const dbPool = createPool({ connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL });
+    const dbPool = new Pool({ connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL });
     const client = await dbPool.connect();
     
     // Create a 150KB string
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     const result = await client.query("SELECT \::text as success", [bigStr]);
     client.release();
     
-    res.json({ result: "success", length: result.rows[0].success.length });
+    res.json({ result: "success", length: result.rows[0].success.length, driver: "native pg" });
   } catch(e) {
     res.json({ error: e.message, stack: e.stack });
   }
